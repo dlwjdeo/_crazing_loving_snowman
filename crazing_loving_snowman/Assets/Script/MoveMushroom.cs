ddcb3d7dc42f69b9646679f1e8b1333d35b9b2a1
dlwjdeo <87020463+dlwjdeo@users.noreply.github.com>
2022-01-03 14:53:00 +0900

    독버섯

--- crazing_loving_snowman/Assets/Script/MoveMushroom.cs
+++ crazing_loving_snowman/Assets/Script/MoveMushroom.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class MoveMushroom : MonoBehaviour
 {
+    Rigidbody2D rigid;
     public Transform target;
     public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+    public Transform player;
 
-    private void LateUpdate()
+    private void Start()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        Vector2 bottomVec = new Vector2(rigid.position.x, rigid.position.y - 3);
+        Debug.DrawRay(bottomVec, Vector3.down, new Color(0, 255, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(bottomVec, Vector3.down, LayerMask.GetMask("Platform"));
+        if (rayHit.collider != null)
+        {
+
+        }
     }
 }
