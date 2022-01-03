5b57cb46aa5aa9c01cd7245c3a6ce78bb0e66432
kimyena243 <83911745+kimyena243@users.noreply.github.com>
2022-01-03 15:02:38 +0900

    스테이지 해금

---
+++ crazing_loving_snowman/Assets/Script/MoveToNextLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevel : MonoBehaviour
+{
+    public int nextSceneLoad;
+
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 5) 
+            {
+                Debug.Log("일단 여긴 놔둠");
+                
+                
+            }
+            else
+            {
+     
+                if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+                {
+                    PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+                }
+            }
+        }
+    }
+}
